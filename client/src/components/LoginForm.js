b8f8c28520fdb8ae97e4c69245914b3a1f6a59ee
jack.loveday95@gmail.com <jack.loveday95@gmail.com>
2021-11-03 16:31:57 -0400

    Modified signup and login forms

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,14 +1,20 @@
+// Modified LoginForm.js by Jack Loveday
+
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+
+// Import dependencies for apollo and mutations
+import { useMutation } from '@apollo/react-hooks';
+import { LOGIN_USER } from '../utils/mutations';
+
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [loginUser, { error }] = useMutation(LOGIN_USER); // User Login - Jack Loveday
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -18,23 +24,23 @@ const LoginForm = () => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-
+    // Adjust Try to check for login and work with apollo/gql - Jack Loveday
     try {
-      const response = await loginUser(userFormData);
 
+      // Check user login
+      const { data } = await loginUser({
+        variables: { ...userFormData }
+      });
+
+      // Login with token
+      Auth.login(data.login.token)
+
+      // Check if response is valid
       if (!response.ok) {
-        throw new Error('something went wrong!');
+        throw new Error('Login is invalid');
       }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      // Catch error
     } catch (err) {
       console.error(err);
       setShowAlert(true);
