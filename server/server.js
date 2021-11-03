2055bc7a0693688f932ca7171f29d5ba7b51ce01
jack.loveday95@gmail.com <jack.loveday95@gmail.com>
2021-11-03 14:10:32 -0400

    Edited server.js and added most expected functions besides unfinished schmea

--- server/server.js
+++ server/server.js
@@ -1,11 +1,30 @@
+// Modified server.js by Jack Loveday
+
+// Import dependencies
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Setup the Apollo server
+const aServer = new ApolloServer({
+  // Pass in Schema data here later
+
+  context: authMiddleware
+})
+
+
+// Init the server
+aServer.applyMiddleware({ app });
+
+// Connect other middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,8 +33,26 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+// Build html page
+app.get('*', (req, res) => {
+  res.sendFile(path
+    .join(
+      __dirname,
+      '../client/build/index.html'
+    ));
+});
+
+// Remove later?
 app.use(routes);
 
+// Edit server run/listen
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+
+  // Open up function to add needed edits
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+
+    // Then Log GraphQL log
+    console.log(`Use GraphQL at http://localhost:${PORT}${aServer.graphqlPath}`);
+  });
 });
